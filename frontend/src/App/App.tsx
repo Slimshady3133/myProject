eb0d020d3f158a9501f07c72813260576ac55edf
Slimshady3133 <i.puzakov2010@mail.ru>
2023-03-07 14:26:48 +0300

    MainPage without css

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Footer from '../Components/Footer/Footer';
 import Header from '../Components/Header/Header';
+import MainPage from '../Components/MainPage/MainPage';
 
 function App(): JSX.Element {
   return (
     <div className="main">
       <Header />
-      <Routes>{/* <Route index element={<Footer />} /> */}</Routes>
+      <Routes>
+        <Route index element={<MainPage />} />
+      </Routes>
       <Footer />
     </div>
   );
